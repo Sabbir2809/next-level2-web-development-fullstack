530aad59bb40f0cad652f271d371929e27e1c32b
Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2024-01-12 21:51:06 +0600

    [Module-3]: Hands-on with redux toolkit

---
+++ Module-3/todo-app-with-redux-toolkit/src/components/todo/TodoFilter.tsx
@@ -0,0 +1,32 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function TodoFilter() {
+  const [position, setPosition] = React.useState("Hight");
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="bg-primary-gradient text-xl font-semibold">Filter</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Filter by priority</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="high">Hight</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="medium">Medium</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
