7526752d66dfc539ef9ccf7ca353cfd393a47d08
sabbir2809 <sabbirto13@gmail.com>
2024-02-27 21:44:14 +0600

    [Module-18]: PostgreSQL Fundamentals and Advanced Data Manipulation Techniques

--- Mission-7/Module-18/select.sql
+++ Mission-7/Module-18/select.sql
@@ -1,4 +1,4 @@
--- Active: 1709008197463@@127.0.0.1@5432@ums
+-- Active: 1709038877792@@127.0.0.1@5432@ums
 
 -- create a new table schema
 CREATE TABLE students (
@@ -15,9 +15,8 @@ CREATE TABLE students (
 );
 
 -- insert to into students table
-INSERT INTO students (first_name, last_name, age, email, dob, blood_group, country, grade, course); 
+INSERT INTO students (first_name, last_name, age, email, dob, blood_group, country, grade, course) 
   VALUES
-  ('Mohammad', 'Rahman', 20, 'mrahman@example.com', '2003-08-15', 'A+', 'Bangladesh', 'A', 'Computer Science'),
   ('Fatima', 'Akhtar', 21, 'fakhtar@example.com', '2002-05-20', 'B-', 'Bangladesh', 'B', 'Electrical Engineering'),
   ('Ahmed', 'Hossain', 19, 'ahossain@example.com', '2003-12-10', 'O+', 'Bangladesh', 'A', 'Mechanical Engineering'),
   ('Sadia', 'Khan', 22, 'skhan@example.com', '2000-10-03', 'AB-', 'Bangladesh', 'B', 'Civil Engineering'),
@@ -26,7 +25,9 @@ INSERT INTO students (first_name, last_name, age, email, dob, blood_group, count
   ('Nadia', 'Chowdhury', 21, 'nchowdhury@example.com', '2001-07-08', 'O-', 'Bangladesh', 'A', 'Dentistry'),
   ('Imran', 'Kabir', 19, 'ikabir@example.com', '2003-01-30', 'A+', 'Bangladesh', 'B', 'Business Administration'),
   ('Ayesha', 'Sultana', 20, 'asultana@example.com', '2002-09-12', 'AB+', 'Bangladesh', 'C', 'Economics'),
-  ('Farhan', 'Islam', 22, 'fislam@example.com', '2000-04-27', 'B-', 'Bangladesh', 'A', 'English Literature');
+  ('Farhan', 'Islam', 22, 'fislam@example.com', '2000-04-27', 'B-', 'Bangladesh', 'A', 'English Literature'),
+  ('Sabbir', 'Hossain', 25, 'sabbir@example.com', '1998-04-27', 'A+', 'Bangladesh', 'A+', 'CSE'),
+  ('Abu', 'Salkin', 24, 'salkin@example.com', '200-04-27', 'B+', 'Bangladesh', 'B+', 'CSE');
 
 
 -- select all data form students table
@@ -56,3 +57,63 @@ SELECT * FROM students
 
 SELECT * FROM students
   WHERE age <> 20;
+
+-- scalar functions [upper(), lower(), concat(), length()]
+SELECT upper(first_name) as "First Name" FROM students;
+SELECT concat(first_name, ' ', last_name) as "Full Name" FROM students;
+SELECT length(first_name) as "Full Name (Length)" FROM students;
+
+-- aggregate functions [avg(), max(), min(), sum(), count()]
+SELECT avg(age) from students;
+SELECT min(age) as "Min Age" from students;
+SELECT max(age) as "Max Age" from students;
+SELECT sum(age) as "Total Age" from students;
+SELECT count(age) as "Student Count" from students;
+
+-- combine aggregate and scalar
+SELECT max(length(first_name)) as "Full Name (Length)" FROM students;
+
+-- logical negation NOT, understanding NULL and the Null-Coalescing
+SELECT * FROM students
+  WHERE NOT grade = 'A';
+
+SELECT * FROM students
+  WHERE email is NULL;
+
+SELECT COALESCE(email,'N/A') as "Email" FROM students;
+
+-- Exploring IN, BETWEEN, LIKE, and ILIKE Operators
+SELECT * FROM students
+  WHERE grade NOT IN('A+', 'A');
+
+SELECT * FROM students
+  WHERE age BETWEEN 18 and 20;
+
+
+SELECT * FROM students
+  WHERE dob BETWEEN '2000-01-01' and '2002-01-01' ORDER BY dob LIMIT 2;
+
+SELECT * FROM students
+  WHERE first_name LIKE '%ir';
+
+SELECT * FROM students
+  WHERE first_name LIKE '%s%';
+
+SELECT * FROM students
+  WHERE first_name ILIKE 's%';
+
+SELECT * FROM students
+  WHERE first_name LIKE '__bb%';
+
+-- Pagination with limit offset and data deletion
+SELECT * FROM students LIMIT 5 OFFSET 5 * 1;
+
+DELETE FROM students
+  WHERE age = 24 and email = 'salkin@example.com';
+
+SELECT * FROM students;
+
+-- update operators
+UPDATE students
+  SET email = 'sabbirto@gmail.com', dob = '1998-06-07'
+  WHERE student_id = 31
