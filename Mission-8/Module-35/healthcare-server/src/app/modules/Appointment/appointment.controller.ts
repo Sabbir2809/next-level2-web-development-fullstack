5a3a1a810d2e18305d4e54c91270ea03ec97383f
Sabbir2809 <sabbirto13@gmail.com>
2024-04-08 23:31:50 +0600

    [Module-35]: Construction of Healthcare Server Part-9

--- Mission-8/Module-35/healthcare-server/src/app/modules/Appointment/appointment.controller.ts
+++ Mission-8/Module-35/healthcare-server/src/app/modules/Appointment/appointment.controller.ts
@@ -1,7 +1,9 @@
 import httpStatus from "http-status";
 import { JwtPayload } from "jsonwebtoken";
 import catchAsync from "../../utils/catchAsync";
+import pick from "../../utils/pick";
 import sendResponse from "../../utils/sendResponse";
+import { appointmentFilterableFields } from "./appointment.constant";
 import { AppointmentServices } from "./appointment.service";
 
 const createAppointment = catchAsync(async (req, res) => {
@@ -16,6 +18,38 @@ const createAppointment = catchAsync(async (req, res) => {
   });
 });
 
+const getMyAppointment = catchAsync(async (req, res) => {
+  const user = (req as JwtPayload).user;
+  const filters = pick(req.query, ["status", "paymentStatus"]);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+  const result = await AppointmentServices.getMyAppointmentFromDB(user, filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Get My Appointment Fetched Successfully",
+    data: result,
+  });
+});
+
+const getAllAppointments = catchAsync(async (req, res) => {
+  const user = (req as JwtPayload).user;
+  const filters = pick(req.query, appointmentFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+  const result = await AppointmentServices.getAllAppointmentsFromDB(user, filters, options);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Get All Appointments Fetched Successfully",
+    data: result,
+  });
+});
+
 export const AppointmentControllers = {
   createAppointment,
+  getMyAppointment,
+  getAllAppointments,
 };
--- Mission-8/Module-35/healthcare-server/src/app/modules/Appointment/appointment.service.ts
+++ Mission-8/Module-35/healthcare-server/src/app/modules/Appointment/appointment.service.ts
@@ -1,5 +1,8 @@
+import { Prisma, UserRole } from "@prisma/client";
 import { JwtPayload } from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
+import { TPaginationOptions } from "../../types/pagination";
+import calculatePagination from "../../utils/pagination";
 import prisma from "../../utils/prisma";
 
 const createAppointmentIntoDB = async (user: JwtPayload, payload: any) => {
@@ -80,6 +83,150 @@ const createAppointmentIntoDB = async (user: JwtPayload, payload: any) => {
   return result;
 };
 
+const getMyAppointmentFromDB = async (
+  user: JwtPayload,
+  filters: any,
+  options: TPaginationOptions
+) => {
+  const { ...filterData } = filters;
+  const { page, limit, skip } = calculatePagination(options);
+  const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+  if (user?.role === UserRole.PATIENT) {
+    andConditions.push({
+      patient: {
+        email: user.email,
+      },
+    });
+  } else if (user?.role === UserRole.DOCTOR) {
+    andConditions.push({
+      doctor: {
+        email: user.email,
+      },
+    });
+  }
+
+  // specific field
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AppointmentWhereInput = { AND: andConditions };
+  const result = await prisma.appointment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+    include:
+      user.role === UserRole.PATIENT
+        ? { doctor: true, schedule: true }
+        : {
+            patient: {
+              include: {
+                patientHealthData: true,
+                medicalReport: true,
+              },
+            },
+            schedule: true,
+          },
+  });
+
+  const total = await prisma.appointment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const getAllAppointmentsFromDB = async (
+  user: JwtPayload,
+  filters: any,
+  options: TPaginationOptions
+) => {
+  const { patientEmail, doctorEmail, ...filterData } = filters;
+  const { page, limit, skip } = calculatePagination(options);
+  const andConditions: Prisma.AppointmentWhereInput[] = [];
+
+  if (patientEmail) {
+    andConditions.push({
+      patient: {
+        email: patientEmail,
+      },
+    });
+  } else if (doctorEmail) {
+    andConditions.push({
+      doctor: {
+        email: doctorEmail,
+      },
+    });
+  }
+
+  // specific field
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AppointmentWhereInput = { AND: andConditions };
+  const result = await prisma.appointment.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      doctor: true,
+      patient: true,
+    },
+  });
+
+  const total = await prisma.appointment.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const AppointmentServices = {
   createAppointmentIntoDB,
+  getMyAppointmentFromDB,
+  getAllAppointmentsFromDB,
 };
