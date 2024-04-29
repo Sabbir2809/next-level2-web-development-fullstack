69a1edd89ca918a06055633b757ca71bfda5001b
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-04-29 12:47:26 +0600

    [Module-43]: Building a Next.js Healthcare Project Part-2

--- Mission-9/Module-43/healthcare-client/src/app/(withCommonLayout)/page.tsx
+++ Mission-9/Module-43/healthcare-client/src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@mui/material";
-
 const HomePage = () => {
   return (
     <div>
       <h1>This is HomePage Component</h1>
-      <Button variant="contained">Contained</Button>
     </div>
   );
 };
