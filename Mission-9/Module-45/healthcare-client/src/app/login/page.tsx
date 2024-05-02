8e0846b6acfeafabef207cf5a05109657005f1e6
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2024-05-02 22:38:16 +0600

    [Module-45]: Building a Next.js Healthcare Project Part-2

--- Mission-9/Module-45/healthcare-client/src/app/login/page.tsx
+++ Mission-9/Module-45/healthcare-client/src/app/login/page.tsx
@@ -1,8 +1,87 @@
+import logo from "@/assets/icons/charity-icon.png";
+import { Box, Button, Container, Grid, Link, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>This is LoginPage Component</h1>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            padding: 4,
+            textAlign: "center",
+          }}>
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+            <Box>
+              <Image src={logo} alt="Logo" width={50} height={50} />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Login Healthcare
+              </Typography>
+            </Box>
+          </Stack>
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="email"
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Typography component="p" fontWeight={300} textAlign="end" marginBottom="1">
+                <Link href="/forget-password" className="text-blue-500">
+                  Forget Password?
+                </Link>
+              </Typography>
+
+              <Button
+                fullWidth={true}
+                sx={{
+                  margin: "10px 0px",
+                }}>
+                Login
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Don&apos;t have an account?{" "}
+                <Link href="/register" className="text-blue-500">
+                  Register
+                </Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
--- Mission-9/Module-45/healthcare-client/src/app/register/page.tsx
+++ Mission-9/Module-45/healthcare-client/src/app/register/page.tsx
@@ -1,8 +1,116 @@
+import logo from "@/assets/icons/charity-icon.png";
+import { Box, Button, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>This is RegisterPage Component</h1>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}>
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            padding: 4,
+            textAlign: "center",
+          }}>
+          {/* Image */}
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}>
+            <Box>
+              <Image src={logo} alt="Logo" width={50} height={50} />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Patient Register
+              </Typography>
+            </Box>
+          </Stack>
+          {/* Input */}
+          <Box>
+            <form>
+              <Grid container spacing={2} my={1}>
+                <Grid item md={12}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="email"
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="tel"
+                    label="Phone"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <TextField
+                    id="outlined-basic"
+                    type="text"
+                    label="Address"
+                    variant="outlined"
+                    size="small"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                fullWidth={true}
+                sx={{
+                  margin: "10px 0px",
+                }}>
+                Register
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Do you already have an account?{" "}
+                <Link href="/login" className="text-blue-500">
+                  Login
+                </Link>
+              </Typography>
+            </form>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
